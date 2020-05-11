096f569f48e6cf1a35695e7a118fb1c83159df5a
httpLucasReis <manoel.reisiema@gmail.com>
2020-05-10 23:01:14 -0300

    pause fix

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,46 @@
 const d_seconds = document.querySelector("div#seconds");
 
-var buffer_seconds;
-var seconds;
+var buffer_seconds;  // Memória padrão para os segundos 
+var seconds;         
 var minuts;
-var toCount = false;
-var checkClick = false;
-var paused = toCount == false && checkClick == true;
+var toCount = false;     // Contando?
+var checkClick = false;  // Clicou?
+
+
+// Função para inserir um elemento na tela 
+function display(element){
+  document.getElementById(element).style.display = 'inline';
+}
+
+// Função para remover um elemento da tela
+function remove(element){
+  document.getElementById(element).style.display = 'none';
+}
+
+
+// Função para verificar se a contagem está pausada
+function paused(){
+  let p = toCount == false && checkClick == true;
+  if(p){
+    alert('Por favor, despause!');
+  }
+}
+
+
+// Função para checar qual botão o usuário clicou
 
 function check(start) {
+
+  if(start == "stop"){
+    display("continue");
+    remove("pause")
+  } 
+
+  if(start == "continue"){
+    display("pause");
+    remove("continue");
+  }
+
   switch (start) {
     case "work":
       work();
@@ -23,18 +56,21 @@ function check(start) {
       break;
   }
 }
-
+// Função para chamar a contagem específica para um ciclo de trabalho
 function work() {
-  buffer_seconds = 1500;
-  if (checkClick == false) {
+  paused();
+  buffer_seconds = 1500;       // 25 minutos em segundos
+  if (checkClick == false) {   
     toCount = true;
     couting();
     checkClick = true;
-  }
+  } 
 }
 
+// Função para chamar a contagem específica para um cliclo de descanso
 function rest() {
-  buffer_seconds = 300;
+  paused();
+  buffer_seconds = 300;      // 5 minutos em segundos
   if (checkClick == false) {
     toCount = true;
     couting();
@@ -42,14 +78,18 @@ function rest() {
   }
 }
 
+// Função que pausa a contagem
 function pause() {
   toCount = false;
 }
 
+// Função que despausa a contagem
 function continueWork() {
   toCount = true;
 }
 
+
+// Transforma os segundos e minutos e os colaca na tela. Chama a função de contagem principal
 function couting() {
   seconds = buffer_seconds % 60;
   minuts = Math.floor(buffer_seconds / 60);
@@ -66,6 +106,7 @@ function couting() {
   setInterval(count, 1000);
 }
 
+// Tira 1 segundo do buffer_seconds e depois reimprime os minutos na tela 
 function count() {
   if (buffer_seconds > 0) {
     if (toCount == true) {
